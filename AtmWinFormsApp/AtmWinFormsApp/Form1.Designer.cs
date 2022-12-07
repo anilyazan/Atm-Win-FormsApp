bfdf7c056622c64b92f5b03effeb3bfd481721cf
anilyazan <117682198+anilyazan@users.noreply.github.com>
2022-12-07 15:29:14 +0300

    Atm Win Forms App
    Atm Win Forms App

---
+++ AtmWinFormsApp/AtmWinFormsApp/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace AtmWinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lbl_User = new System.Windows.Forms.Label();
+            this.lbl_pass = new System.Windows.Forms.Label();
+            this.txt_User = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.Location = new System.Drawing.Point(284, 334);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(234, 68);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Giriş Yap";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lbl_User
+            // 
+            this.lbl_User.AutoSize = true;
+            this.lbl_User.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_User.Location = new System.Drawing.Point(212, 110);
+            this.lbl_User.Name = "lbl_User";
+            this.lbl_User.Size = new System.Drawing.Size(130, 25);
+            this.lbl_User.TabIndex = 1;
+            this.lbl_User.Text = "Kullanıcı Adı :";
+            // 
+            // lbl_pass
+            // 
+            this.lbl_pass.AutoSize = true;
+            this.lbl_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_pass.Location = new System.Drawing.Point(279, 216);
+            this.lbl_pass.Name = "lbl_pass";
+            this.lbl_pass.Size = new System.Drawing.Size(63, 25);
+            this.lbl_pass.TabIndex = 2;
+            this.lbl_pass.Text = "Şifre :";
+            // 
+            // txt_User
+            // 
+            this.txt_User.Location = new System.Drawing.Point(381, 113);
+            this.txt_User.Name = "txt_User";
+            this.txt_User.Size = new System.Drawing.Size(168, 22);
+            this.txt_User.TabIndex = 3;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(381, 220);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(168, 22);
+            this.txt_pass.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(796, 450);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_User);
+            this.Controls.Add(this.lbl_pass);
+            this.Controls.Add(this.lbl_User);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lbl_User;
+        private System.Windows.Forms.Label lbl_pass;
+        private System.Windows.Forms.TextBox txt_User;
+        private System.Windows.Forms.TextBox txt_pass;
+    }
+}
+
---
+++ AtmWinFormsApp/AtmWinFormsApp/Form2.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AtmWinFormsApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_parayatir = new System.Windows.Forms.Button();
+            this.btn_paracekme = new System.Windows.Forms.Button();
+            this.lbl_bakiye = new System.Windows.Forms.Label();
+            this.lbl_paramiz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_parayatir
+            // 
+            this.btn_parayatir.Location = new System.Drawing.Point(329, 253);
+            this.btn_parayatir.Name = "btn_parayatir";
+            this.btn_parayatir.Size = new System.Drawing.Size(152, 52);
+            this.btn_parayatir.TabIndex = 0;
+            this.btn_parayatir.Text = "Para yatırma";
+            this.btn_parayatir.UseVisualStyleBackColor = true;
+            // 
+            // btn_paracekme
+            // 
+            this.btn_paracekme.Location = new System.Drawing.Point(329, 335);
+            this.btn_paracekme.Name = "btn_paracekme";
+            this.btn_paracekme.Size = new System.Drawing.Size(152, 52);
+            this.btn_paracekme.TabIndex = 1;
+            this.btn_paracekme.Text = "Para çekme";
+            this.btn_paracekme.UseVisualStyleBackColor = true;
+            // 
+            // lbl_bakiye
+            // 
+            this.lbl_bakiye.AutoSize = true;
+            this.lbl_bakiye.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_bakiye.Location = new System.Drawing.Point(326, 112);
+            this.lbl_bakiye.Name = "lbl_bakiye";
+            this.lbl_bakiye.Size = new System.Drawing.Size(82, 25);
+            this.lbl_bakiye.TabIndex = 2;
+            this.lbl_bakiye.Text = "Bakiye :";
+            // 
+            // lbl_paramiz
+            // 
+            this.lbl_paramiz.AutoSize = true;
+            this.lbl_paramiz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_paramiz.Location = new System.Drawing.Point(460, 112);
+            this.lbl_paramiz.Name = "lbl_paramiz";
+            this.lbl_paramiz.Size = new System.Drawing.Size(56, 25);
+            this.lbl_paramiz.TabIndex = 3;
+            this.lbl_paramiz.Text = "2000";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_paramiz);
+            this.Controls.Add(this.lbl_bakiye);
+            this.Controls.Add(this.btn_paracekme);
+            this.Controls.Add(this.btn_parayatir);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_parayatir;
+        private System.Windows.Forms.Button btn_paracekme;
+        private System.Windows.Forms.Label lbl_bakiye;
+        private System.Windows.Forms.Label lbl_paramiz;
+    }
+}
